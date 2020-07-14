57a5028fdc89ff0b1ec8d1aef1b25462996318e5
Erick Mora <erick_3069@hotmail.com>
2020-07-14 08:59:05 -0500

    aranias

---
+++ 03-spyder/a_introduccion.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 07:51:42 2020
+
+@author: erick
+"""
+
+
+print("Hola spider")
+
+nombre = "Erick"
+numero = 3
+lista = [1,2,3]
+tupla = (4,5,6)
+diccionario = {
+    "nombre": "Erick",
+    "apellido": "Mora"
+    
+    }
