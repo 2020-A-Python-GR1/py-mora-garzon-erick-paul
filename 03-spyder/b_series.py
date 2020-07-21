29501ae4e816197c2ad0f0967fe1d8768270d5be
Erick Mora <erick_3069@hotmail.com>
2020-07-21 09:02:31 -0500

    series  pandas

--- 03-spyder/b_series.py
+++ 03-spyder/b_series.py
@@ -79,4 +79,123 @@ serie_valor_ciudad["Quito"] = serie_valor_ciudad["Quito"] - 50
 print("Lima" in serie_valor_ciudad)
 
 
+svc_cuadrado = np.square(serie_valor_ciudad)
+
+ciudades_uno = pd.Series({
+    "Montanita": 300,
+    "Guayaquil": 10000,
+    "Quito": 2000
+    })
+
+ciudades_dos = pd.Series({
+    "Loja": 300,
+    "Guayaquil": 10000
+    })
+
+ciudades_uno["Loja"] = 0
+
+print(ciudades_uno + ciudades_dos)
+print(type(ciudades_uno + ciudades_dos))
+
+ciudades_add = ciudades_uno.add(ciudades_dos)
+#.sub()
+#.mul()
+#.div()
+
+
+
+ciud_concat = pd.concat([
+    ciudades_uno,
+    ciudades_dos
+    ])
+
+# ciud_concat_verify = pd.concat([
+#     ciudades_uno,
+#     ciudades_dos
+#     ],
+#     verify_integrity = True)
+
+# ciud_append_verify = pd.append([ #append y concat hacen lo mismo
+#     ciudades_uno,
+#     ciudades_dos
+#     ],
+#     verify_integrity = True)
+
+ciud_concat_verify2 = pd.concat([
+    ciudades_uno,
+    ciudades_dos
+    ],
+    verify_integrity = False)
+
+# ciud_append_verify2 = pd.append([ #append y concat hacen lo mismo
+#     ciudades_uno,
+#     ciudades_dos
+#     ],
+#     verify_integrity = False)
+
+ciud_append_verify3 = ciudades_uno.append( #append y concat hacen lo mismo
+    ciudades_dos,
+    verify_integrity = False)
+
+print(ciudades_uno.max())
+print(pd.Series.max(ciudades_uno))
+print(np.max(ciudades_uno))
+
+print(ciudades_uno.min())
+print(pd.Series.min(ciudades_uno))
+print(np.min(ciudades_uno))
+
+print(ciudades_uno.mean())  #promedio
+print(ciudades_uno.median())    #mediana
+print(np.average(ciudades_uno)) #promedio
+
+print(ciudades_uno.head(2))
+print(ciudades_uno.tail(2))
+
+
+print(ciudades_uno.sort_values(
+    ascending = False).head(2))
+
+print(ciudades_uno.sort_values().tail(2))
+
+def calcular(valor_serie):
+    if(valor_serie <=1000):
+        return valor_serie * 1.05
+    if(valor_serie > 1000 and valor_serie <= 5000):
+        return valor_serie * 1.10
+    if(valor_serie > 5000):
+        return valor_serie * 1.15
+    
+ciudad_calculada = ciudades_uno.map(calcular)
+
+
+#if else
+# cuando no cumple condicion aplica
+
+resultado = ciudades_uno.where(ciudades_uno < 1000,
+                   ciudades_uno * 1.05)
+
+
+series_numeros = pd.Series(['1.0', '2', -3])
+print(pd.to_numeric(series_numeros))
+
+# 'integer', 'signed', 'unsigned', 'float'
+print(pd.to_numeric(series_numeros, downcast = 'integer'))
+
+series_numeros_err = pd.Series(['no tiene', '1.0', '2', -3])
+
+# ignore, coerce, raise (default)
+# print(pd.to_numeric(series_numeros_err))
+print(pd.to_numeric(series_numeros_err, errors='ignore'))
+print(pd.to_numeric(series_numeros_err, errors='coerce'))
+
+
+
+
+
+
+
+
+
+
 
